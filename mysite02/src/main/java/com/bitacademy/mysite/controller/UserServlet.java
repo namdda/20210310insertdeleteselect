aac3a75c93be7221c57fdaff34d81b5c053e4c35
BIT_R34 <my5475@naver.com>
2021-03-12 10:19:15 +0900

    login cookie

--- mysite02/src/main/java/com/bitacademy/mysite/controller/UserServlet.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/UserServlet.java
@@ -14,36 +14,58 @@ import com.bitacademy.web.mvc.WebUtil;
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		
-		String action = request.getParameter("a"); // 요청 받는거 구분하기 위해 
-		
-		if("joinform".equals(action)) {
+
+		String action = request.getParameter("a"); // 요청 받는거 구분하기 위해
+
+		if ("joinform".equals(action)) {
 			WebUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
-		} else if("joinsuccess".equals(action)) {
+		} else if ("joinsuccess".equals(action)) {
 			WebUtil.forward("/WEB-INF/views/user/joinsuccess.jsp", request, response);
-		} else if("join".equals(action)) {
+		} else if ("join".equals(action)) {
 			String name = request.getParameter("name");
 			String email = request.getParameter("email");
 			String password = request.getParameter("password");
 			String gender = request.getParameter("gender");
-			
+
 			UserVo userVo = new UserVo();
 			userVo.setName(name);
 			userVo.setEmail(email);
 			userVo.setPassword(password);
 			userVo.setGender(gender);
-			
+
 			new UserDao().insert(userVo);
-			
+
 			WebUtil.redirect(request.getContextPath() + "/user?a=joinsuccess", request, response);
+		} else if("loginform".equals(action)) {
+			WebUtil.forward("/WEB-INF/views/user/loginform.jsp", request, response);
+		} else if("login".equals(action)) {
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			
+			UserVo vo = new UserVo();
+			vo.setEmail(email);
+			vo.setPassword(password);
+			
+			UserVo authUser = new UserDao().findByEmailAndPassword(vo);
+			if(authUser == null) {
+				request.setAttribute("authResult", "fail");
+				WebUtil.forward("/WEB-INF/views/user/loginform.jsp", request, response);
+				return;	
+			} 			
+			
+			// 인증 처리
+			
+			
 		} else {
 			WebUtil.redirect(request.getContextPath(), request, response);
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
